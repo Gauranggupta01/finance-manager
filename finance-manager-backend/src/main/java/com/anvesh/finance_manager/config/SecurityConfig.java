3628a2b2a704542c09c4416160ddf7497a793317
Gaurang Gupta <guptagaurang0112@gmail.com>
2026-05-26 16:45:58 +0530

    update security

--- finance-manager-backend/src/main/java/com/anvesh/finance_manager/config/SecurityConfig.java
+++ finance-manager-backend/src/main/java/com/anvesh/finance_manager/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.security.web.SecurityFilterChain;
 
 import org.springframework.web.cors.CorsConfiguration;
@@ -35,6 +37,12 @@ public class SecurityConfig {
             );
 
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
